8e2909371a2e981c458c14d5c3168860e058f760
Audric23 <audric.sartoris@gmail.com>
2023-06-30 00:23:11 +0200

    First commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function()
+{
+    document.querySelector("#flag_text").onclick = function()
+   {
+     document.getElementById("flag").hidden = false;
+   }
+})
